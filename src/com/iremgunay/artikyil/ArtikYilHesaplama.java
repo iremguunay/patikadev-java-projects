5a76a67383ddb7a2f6eb587730b2236f69f837b7
Irem Gunay <iremguunay@gmail.com>
2022-06-08 00:19:32 +0300

    ArtikYilHesaplayan program

---
+++ src/com/iremgunay/artikyil/ArtikYilHesaplama.java
@@ -0,0 +1,29 @@
+package com.iremgunay.artikyil;
+
+import java.util.Scanner;
+
+public class ArtikYilHesaplama {
+    public static void main(String[] args) {
+        int year;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Yılı giriniz: ");
+        year = input.nextInt();
+
+        if(year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + " bir artık yıldır.");
+                } else {
+                    System.out.println(year + " bir artık yıl değildir.");
+                }
+            } else {
+                System.out.println(year + " bir artık yıldır.");
+            }
+        } else {
+            System.out.println(year + " bir artık yıl değildir.");
+        }
+
+    }
+}
